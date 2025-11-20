219fd45719af1cd523b060bbef5450d9c3e33c57
Martyn Leeper <8573821+MartynMartyn@users.noreply.github.com>
2025-11-20 03:39:09 +0000

    attempt 2

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1763609293',resources: [
+    cacheName: 'chirpy-1763609926',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
