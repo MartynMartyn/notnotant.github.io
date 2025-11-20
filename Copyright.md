db4a177c6edcb28296497db1c4e707ceaca90abc
Martyn Leeper <8573821+MartynMartyn@users.noreply.github.com>
2025-11-20 03:36:35 +0000

    Updating portfolio website
    attempting to merge in portfolio website changes

--- Copyright.md
+++
@@ -1,2 +0,0 @@
-Copyright (c) 2019 Martyn Leeper. All rights reserved.
-All content rights are reserved.
---
+++ assets/js/data/swconf.js
@@ -0,0 +1,32 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1763609293',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/projects/',
+      
+        '/categories/Work',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
