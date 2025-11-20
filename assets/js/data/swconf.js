407e8ec8f5f03c0045ad7b4096d3e2d888ddac29
Martyn Leeper <8573821+MartynMartyn@users.noreply.github.com>
2025-11-20 03:51:14 +0000

    remove base url

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1763610493',resources: [
+    cacheName: 'chirpy-1763610651',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
